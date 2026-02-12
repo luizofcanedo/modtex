5d65a249f1ca57a92a58c678791a0bd0db7b87e9
Luiz Otávio <luizofcanedo@hotmail.com>
2026-02-11 22:40:34 -0300

    -> Botões agora funcionam e criam um loop infinito onde você pode escolher infinitamente qual palavra mais se relaciona com a outra
    Futuros updates:
    -> Quando uma palavra selecionada possui lift negativo, a pagina buga e fica infinitamente carregando, então arrumar isso.
    -> Implementar a dinâmica das palavras relacionadas pras palavras bot
    -> Limpar o código e deixar tudo em defs invés de hardcode

--- words_list.py
+++ words_list.py
@@ -16,15 +16,15 @@ url_object = URL.create(
 engine = create_engine(url_object)
 
 query = """
-    SELECT
-        "ac_des_atendimento" as ordem_servico, "ac_nom_marca" as marca, "os_num_km_atual" as km_medio
-    FROM
-        public.grow_os;
-"""
+        SELECT "ac_des_atendimento" as ordem_servico, \
+               "ac_nom_marca"       as marca, \
+               "os_num_km_atual"    as km_medio
+        FROM public.grow_os; \
+        """
+
 
 @st.cache_data(ttl=3600)
 def contagem_palavras(filter=False, target_word=None, marca_alvo=None, sorted=False):
-
     if filter == False:
         df = pd.read_sql(query, engine)
 
@@ -40,14 +40,15 @@ def contagem_palavras(filter=False, target_word=None, marca_alvo=None, sorted=Fa
         df_contagem = df_clean.groupby(['marca', 'words']).size().reset_index(name='contagem')
 
         return df_contagem
-    
+
     else:
         df = pd.read_sql(query, engine)
 
         df_filtrado_palavra = df[df['ordem_servico'].str.contains(target_word, case=False)]
 
         df_filtrado_palavra['ordem_servico'] = df_filtrado_palavra['ordem_servico'].astype(str).str.lower()
-        df_exploded = df_filtrado_palavra.assign(words=df_filtrado_palavra['ordem_servico'].str.split()).explode('words')
+        df_exploded = df_filtrado_palavra.assign(words=df_filtrado_palavra['ordem_servico'].str.split()).explode(
+            'words')
         df_exploded = df_exploded.dropna(subset=['words'])
         df_exploded = df_exploded[df_exploded['words'] != target_word]
 
@@ -59,32 +60,34 @@ def contagem_palavras(filter=False, target_word=None, marca_alvo=None, sorted=Fa
         df_contagem = df_clean.groupby(['words']).size().reset_index(name='contagem')
 
         if sorted == True:
-            df_sorted = df_contagem.sort_values(by='contagem',ascending=False).head(10)
-        
+            df_sorted = df_contagem.sort_values(by='contagem', ascending=False).head(10)
+
         return df_sorted
+
 
 def calcular_lift_por_marca(marca_alvo, n, sort=bool):
     df_completo = contagem_palavras()
 
     matriz = df_completo.pivot_table(
         index='words',
-        columns='marca', 
-        values='contagem', 
+        columns='marca',
+        values='contagem',
         fill_value=0
     )
-    
+
     matriz = matriz[matriz.sum(axis=1) > 50]
 
     smoothing = 1e-6
-    
-    freq_relativa = (matriz+smoothing).div((matriz+smoothing).sum(axis=0), axis=1)
+
+    freq_relativa = (matriz + smoothing).div((matriz + smoothing).sum(axis=0), axis=1)
     freq_global = freq_relativa.mean(axis=1)
     lift = freq_relativa.div(freq_global, axis=0)
-    
+
     score_log = np.log(lift)
 
     resultado = score_log[marca_alvo].sort_values(ascending=sort).head(n)
     return resultado
+
 
 def km_medio(target_word, marca_alvo):
     df = pd.read_sql(query, engine)
@@ -93,6 +96,7 @@ def km_medio(target_word, marca_alvo):
     km_medio = df_filtrado_marca['km_medio'].mean()
 
     return km_medio
+
 
 def marcas_associadas(target_word, marca_alvo):
     df = pd.read_sql(query, engine)
@@ -129,4 +133,4 @@ def obter_score_palavra(marca_alvo, palavra_alvo):
         score = score_log.loc[palavra_alvo, marca_alvo]
         return score
     except KeyError:
-        return None
+        return 0.0
